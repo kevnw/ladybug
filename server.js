11d1c2d2c0261de2d8fe73c04e9f606c70a0c4c6
Kevin William <kevinwilliam1907@gmail.com>
2020-06-29 01:39:45 +0800

    Deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static('client/build'));
 
   app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://orbital-trackr.herokuapp.com"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Origin", "https://ask-ladybug.herokuapp.com"); // update to match the domain you will make the request from
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
   });
