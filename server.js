658017e5da7765444129888d22f9909a668b621c
Kevin William <kevinwilliam1907@gmail.com>
2020-06-29 01:50:58 +0800

    Deploy

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use(require('./app/routes'))
 if (process.env.NODE_ENV === "production") {
   app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
   app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "https://ask-ladybug.herokuapp.com"); // update to match the domain you will make the request from
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
