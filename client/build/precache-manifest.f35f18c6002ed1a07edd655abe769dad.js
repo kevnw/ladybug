a7ae84190020b3b0e769a0802da94daeb8e420ad
Kevin William <kevinwilliam1907@gmail.com>
2020-06-29 20:08:13 +0800

    Deploy

---
+++ client/build/precache-manifest.f35f18c6002ed1a07edd655abe769dad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f118d7ec6d09052f8e7da05657a78287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9ad8dd4e44420a5cb7",
+    "url": "/static/css/main.5046215a.chunk.css"
+  },
+  {
+    "revision": "358b3544c7e6b6574fe0",
+    "url": "/static/js/2.dab7bcb9.chunk.js"
+  },
+  {
+    "revision": "7e6ec0d9861ce35e695c4f042343a82f",
+    "url": "/static/js/2.dab7bcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9ad8dd4e44420a5cb7",
+    "url": "/static/js/main.e97ffd5e.chunk.js"
+  },
+  {
+    "revision": "77feaa5c9ae9afa76802",
+    "url": "/static/js/runtime-main.c5270683.js"
+  },
+  {
+    "revision": "50f04c8dc7b994075a3c15d0fe753905",
+    "url": "/static/media/background.50f04c8d.jpg"
+  },
+  {
+    "revision": "c1b12fc2ea238f125ab9117de3e70b50",
+    "url": "/static/media/landing.c1b12fc2.jpg"
+  },
+  {
+    "revision": "3d7404c301b92989be17b9b9675b7052",
+    "url": "/static/media/logoweb.3d7404c3.png"
+  },
+  {
+    "revision": "01b3dd8d14ff4634f95bd7efb9d8689b",
+    "url": "/static/media/name_white.01b3dd8d.png"
+  },
+  {
+    "revision": "fcc89a56c3bd81a177ab027077bd8176",
+    "url": "/static/media/verifymail.fcc89a56.png"
+  }
+]);
