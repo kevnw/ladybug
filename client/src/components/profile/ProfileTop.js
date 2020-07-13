050e2b2d1ec8f413c91cb05d15597a1cd4ffe6bb
lucia <luciatirtag@gmail.com>
2020-07-13 23:25:41 +0800

    Edit profile picture

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,104 +1,133 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import Badge from '@material-ui/core/Badge';
 import { Link } from 'react-router-dom';
+import ChangeProfileModal from './ChangeProfileModal';
 
 const ProfileTop = ({ user, profile, setShowingHeader }) => {
+  const [isShowingPhoto, setShowingPhoto] = useState(false);
   return (
-    <div className="ui segment">
-      <div className="ui grid" style={{ backgroundColor: '#b82601' }}>
-        <div
-          className="sixteen wide column center-text white-text"
-          style={{ padding: '2rem' }}
-        >
-          <img
-            className="ui small circular centered image"
-            style={{ position: 'relative' }}
-            src={profile.avatar}
-            alt=""
-          />
-          <div className="splitscreen">
-            <h4 className="hide-buttonA large">
-              {`${profile.name} `}
-              {user._id === profile.user && (
-                <button
-                  onClick={() => setShowingHeader(true)}
-                  className="invisible-button hide-buttonB"
+    <Fragment>
+      <div className="ui segment">
+        <div className="ui grid" style={{ backgroundColor: '#b82601' }}>
+          <div
+            className="sixteen wide column center-text white-text"
+            style={{ padding: '2rem' }}
+          >
+            <div>
+              <Badge
+                overlap="circle"
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                badgeContent={
+                  <button
+                    onClick={() => setShowingPhoto(true)}
+                    className="circular ui icon button"
+                  >
+                    <i className="icon camera"></i>
+                  </button>
+                }
+              >
+                <img
+                  className="ui small circular centered image"
+                  style={{ position: 'relative' }}
+                  src={profile.avatar}
+                  alt=""
+                />
+              </Badge>
+            </div>
+            <div className="splitscreen">
+              <h4 className="hide-buttonA large">
+                {`${profile.name} `}
+                {user._id === profile.user && (
+                  <button
+                    onClick={() => setShowingHeader(true)}
+                    className="invisible-button hide-buttonB"
+                  >
+                    <i
+                      className="ui tiny white icon edit outline"
+                      style={{ color: 'white' }}
+                    ></i>
+                  </button>
+                )}
+              </h4>
+            </div>
+            {profile.status && profile.status != '0' && (
+              <div className="lead">{`${profile.status}`}</div>
+            )}
+            <div className="icons">
+              {profile.social && profile.social.website && (
+                <a
+                  href={`${profile.social.website}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="white-link"
                 >
-                  <i
-                    className="ui tiny white icon edit outline"
-                    style={{ color: 'white' }}
-                  ></i>
-                </button>
+                  <i className="big globe icon"></i>
+                </a>
               )}
-            </h4>
-          </div>
-          {profile.status && profile.status != '0' && (
-            <div className="lead">{`${profile.status}`}</div>
-          )}
-          <div className="icons">
-            {profile.social && profile.social.website && (
-              <a
-                href={`${profile.social.website}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="white-link"
-              >
-                <i className="big globe icon"></i>
-              </a>
-            )}
-            {profile.social && profile.social.twitter && (
-              <a
-                href={`${profile.social.twitter}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="white-link"
-              >
-                <i className="big twitter icon"></i>
-              </a>
-            )}
-            {profile.social && profile.social.facebook && (
-              <a
-                href={`${profile.social.facebook}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="white-link"
-              >
-                <i className="big facebook icon"></i>
-              </a>
-            )}
-            {profile.social && profile.social.linkedin && (
-              <a
-                href={`${profile.social.linkedin}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="white-link"
-              >
-                <i className="big linkedin icon"></i>
-              </a>
-            )}
-            {profile.social && profile.social.youtube && (
-              <a
-                href={profile.social.youtube}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="white-link"
-              >
-                <i className="big youtube icon"></i>
-              </a>
-            )}
-            {profile.social && profile.social.instagram && (
-              <a
-                href={`${profile.social.instagram}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="white-link"
-              >
-                <i className="big instagram icon"></i>
-              </a>
-            )}
+              {profile.social && profile.social.twitter && (
+                <a
+                  href={`${profile.social.twitter}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="white-link"
+                >
+                  <i className="big twitter icon"></i>
+                </a>
+              )}
+              {profile.social && profile.social.facebook && (
+                <a
+                  href={`${profile.social.facebook}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="white-link"
+                >
+                  <i className="big facebook icon"></i>
+                </a>
+              )}
+              {profile.social && profile.social.linkedin && (
+                <a
+                  href={`${profile.social.linkedin}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="white-link"
+                >
+                  <i className="big linkedin icon"></i>
+                </a>
+              )}
+              {profile.social && profile.social.youtube && (
+                <a
+                  href={profile.social.youtube}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="white-link"
+                >
+                  <i className="big youtube icon"></i>
+                </a>
+              )}
+              {profile.social && profile.social.instagram && (
+                <a
+                  href={`${profile.social.instagram}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="white-link"
+                >
+                  <i className="big instagram icon"></i>
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {isShowingPhoto && (
+        <ChangeProfileModal
+          setShowingPhoto={setShowingPhoto}
+          profile={profile}
+        />
+      )}
+    </Fragment>
   );
 };
 
