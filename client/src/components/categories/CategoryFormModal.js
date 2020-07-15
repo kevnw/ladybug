74a8879fb9b38e0f3e7fcfc8b2e07a8e0f6758b6
Kevin William <kevinwilliam1907@gmail.com>
2020-07-15 18:22:12 +0800

    Profile Picture

--- client/src/components/categories/CategoryFormModal.js
+++ client/src/components/categories/CategoryFormModal.js
@@ -13,20 +13,20 @@ const CategoryFormModal = ({ setShowing, addRequest }) => {
     { key: 'au', value: 'australia', flag: 'au', text: 'Australia' },
     { key: 'ca', value: 'canada', flag: 'ca', text: 'Canada' },
     { key: 'cn', value: 'china', flag: 'cn', text: 'China' },
-    { key: 'dk', value: 'Denmark', flag: 'dk', text: 'Denmark' },
-    { key: 'fi', value: 'Finland', flag: 'fi', text: 'Finland' },
-    { key: 'de', value: 'Germany', flag: 'de', text: 'Germany' },
-    { key: 'hk', value: 'HongKong', flag: 'hk', text: 'Hong Kong' },
-    { key: 'in', value: 'India', flag: 'in', text: 'India' },
-    { key: 'id', value: 'Indonesia', flag: 'id', text: 'Indonesia' },
-    { key: 'it', value: 'Italy', flag: 'it', text: 'Italy' },
-    { key: 'jp', value: 'Japan', flag: 'jp', text: 'Japan' },
-    { key: 'my', value: 'Malaysia', flag: 'my', text: 'Malaysia' },
-    { key: 'nl', value: 'Netherlands', flag: 'nl', text: 'Netherlands' },
-    { key: 'nz', value: 'NewZealand', flag: 'nz', text: 'New Zealand' },
-    { key: 'ph', value: 'Philippines', flag: 'ph', text: 'Philippines' },
-    { key: 'sg', value: 'Singapore', flag: 'sg', text: 'Singapore' },
-    { key: 'za', value: 'SouthAfrica', flag: 'za', text: 'South Africa' },
+    { key: 'dk', value: 'denmark', flag: 'dk', text: 'Denmark' },
+    { key: 'fi', value: 'finland', flag: 'fi', text: 'Finland' },
+    { key: 'de', value: 'germany', flag: 'de', text: 'Germany' },
+    { key: 'hk', value: 'hongkong', flag: 'hk', text: 'Hong Kong' },
+    { key: 'in', value: 'india', flag: 'in', text: 'India' },
+    { key: 'id', value: 'indonesia', flag: 'id', text: 'Indonesia' },
+    { key: 'it', value: 'italy', flag: 'it', text: 'Italy' },
+    { key: 'jp', value: 'japan', flag: 'jp', text: 'Japan' },
+    { key: 'my', value: 'malaysia', flag: 'my', text: 'Malaysia' },
+    { key: 'nl', value: 'netherlands', flag: 'nl', text: 'Netherlands' },
+    { key: 'nz', value: 'newzealand', flag: 'nz', text: 'New Zealand' },
+    { key: 'ph', value: 'philippines', flag: 'ph', text: 'Philippines' },
+    { key: 'sg', value: 'singapore', flag: 'sg', text: 'Singapore' },
+    { key: 'za', value: 'southafrica', flag: 'za', text: 'South Africa' },
     { key: 'es', value: 'Spain', flag: 'es', text: 'Spain' },
     { key: 'ch', value: 'Switzerland', flag: 'ch', text: 'Switzerland' },
     { key: 'tw', value: 'Taiwan', flag: 'tw', text: 'Taiwan' },
@@ -49,9 +49,10 @@ const CategoryFormModal = ({ setShowing, addRequest }) => {
   useEffect(() => {
     if (country) {
       var countryCode = country.toLowerCase()
-      const path = 'https://raw.githubusercontent.com/kevnw/ladybug-assets/master/data.json'
+      const path = `/university/${country}.json`
+      // 'https://raw.githubusercontent.com/kevnw/ladybug-assets/master/data.json'
       axios.get(path)
-        .then((response) => setUniList(response.data[0][`${countryCode}`]));
+        .then((response) => setUniList(response.data));
       // console.log(countryCode)
       // setUniList(data[0][`${countryCode}`])
     }
